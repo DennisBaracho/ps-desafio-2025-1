433bba7e13cf9c0cf40e08f23027b1a5321fab40
Dennis <dennis.baracho@edu.ufes.br>
2025-05-14 08:57:57 -0300

    add buyButton states and effects

--- frontend/src/components/site_PS/card/card.tsx
+++ frontend/src/components/site_PS/card/card.tsx
@@ -4,16 +4,31 @@ import '../variables.css'
 import { vehicleType } from '@/types/vehicle'
 import style from './style.module.css'
 import { buyVehicle } from '@/actions/vehicle'
+import { useEffect, useState } from 'react'
 
 interface vehicleProp {
   vehicle: vehicleType
 }
 
 export default function Card({ vehicle }: vehicleProp) {
+  const [buttonValue, setButtonValue] = useState('Comprar')
+  const [amount, setAmount] = useState(vehicle.in_stock)
+  const [soldOut, setSoldOut] = useState(false)
   async function buyVehicles(id: string) {
+    setAmount((prev) => prev - 1)
+    setButtonValue('Comprado')
+
     const response = await buyVehicle(id)
     console.log(response)
   }
+
+  useEffect(() => {
+    if (amount <= 0) {
+      setButtonValue('Esgotado')
+      setSoldOut(true)
+    }
+  }, [amount])
+
   return (
     <div className={style.card}>
       <div className={style.container_image_name}>
@@ -30,10 +45,12 @@ export default function Card({ vehicle }: vehicleProp) {
           Ano de fabricação: {vehicle.model_year}
         </p>
         <p className={style.card_content}>Categoria:{vehicle.category.label}</p>
-        <p className={style.card_content}>Em estoque: {vehicle.in_stock}</p>
+        <p className={style.card_content}>Em estoque: {amount}</p>
+        <p className={style.card_content}>Preço: R${vehicle.price}</p>
         <input
           type="button"
-          defaultValue={'Comprar'}
+          disabled={soldOut}
+          value={buttonValue}
           className={style.buy_button}
           onClick={() => buyVehicles(vehicle.id)}
         ></input>
